daef950396f72cd653896333e99addc86434869f
avoajaugochukwu <karlthegoo@hotmail.com>
2015-03-17 16:47:36 +0100

    addedd image

--- suave/views.py
+++ suave/views.py
@@ -25,7 +25,7 @@ def index(request):
 	return render(request, 'i/index_test.html', context)
 
 
-
+"""register Client"""
 def clientRegister(request):
 	context = {}
 	context['action'] = '/suave/register/'
@@ -33,49 +33,63 @@ def clientRegister(request):
 
 	if request.method == 'POST':
 		user_form = UserForm(request.POST)
-		other_form = ClientRegisterForm(request.POST)
+		client_form = ClientRegisterForm(request.POST)
 
-		if user_form.is_valid() and other_form.is_valid():
-			#DEBUG 
+		if user_form.is_valid() and client_form.is_valid():
+			
 			data = user_form.cleaned_data
+			#DEBUG ----------------
 			print data['username']
 			print data['password']
+			#DEBUG ----------------
+
 			# save user info
 			user_form_data = user_form.save()
 			#hash password with set_password() -> save
 			user_form_data.set_password(user_form_data.password)
 			user_form_data.save()
 
-			#other_data holds other client details like sex preference...
-			other_form_data = other_form.save(commit=False)
-			other_form_data.user = user_form_data
+			#client_data holds other client details like sex || preference...
+			client_form_data = client_form.save(commit=False)
+			#make the client_form_data aware of its one to one relationship with the User
+			client_form_data.user = user_form_data
 
-			# save client extra details
-			other_form_data.save()
+			# save client along with user object
+			client_form_data.save()
 
-
-			 #@todo use this to display message that last for some time to welcome new users (jquery)
+			##user is authenticated and login is automatic immediately after registering
 			user = authenticate(username=data['username'], password=data['password'])
 
 			if user is not None:
 				login(request, user)
+			#DEBUG ----------------
+			if user.is_authenticated():
 				print 'Login worked'
 			else:
 				print 'Login not work'
-			request.session['client_id'] = int(other_form_data.id)
+			#DEBUG ----------------
+
+			request.session['client_id'] = int(client_form_data.id)
+			#DEBUG ----------------
 			print 'request.session', request.session['client_id']
+			#DEBUG ----------------
+			# use ['registered'] to make registration welcome notice display only once||on registration
 			request.session['registered'] = 1
+			#redirect to clientDashboard view and its associated 'dashboard' page
 			return redirect('suave:clientDashboard')
 
 
+		#invalid form return with errors
 		else:
 			context['user_form'] = user_form
-			context['other_form'] = other_form
-
+			context['client_form'] = client_form
+			#DEBUG ----------------
 			print context['user_form'].errors, context['user_form'].errors
+			#DEBUG ----------------
+	#request not post display 'register' page
 	else:
 		context['user_form'] = UserForm()
-		context['other_form'] = ClientRegisterForm()
+		context['client_form'] = ClientRegisterForm()
 
 	return render(request, 'i/client/form.html', context)
 
@@ -86,7 +100,7 @@ def test(request):
 	print me.user.email
 	return HttpResponse('heelllo '+ me.user.username)
 
-@login_required #working
+@login_required
 def clientDashboard(request):
 	##Test for login
 
@@ -133,25 +147,33 @@ def clientHome(request):
 
 def order(request):
 	context = {}
-	# context['action'] = ''
+
 	context['maleSize_form'] = MaleSizeForm()
 	context['femaleSize_form'] = FemaleSizeForm()
 	context['order_form'] = OrderForm()
 	client = Client.objects.get(id=request.session['client_id'])
-	# print client['id']
+
 
 	if request.method == 'POST':
 		sex = request.POST.get('sex')
 		delivery_option = request.POST.get('delivery_option')
-
+		#DEBUG ----------------
 		print delivery_option
-
+		#DEBUG ----------------
+		"""
+		order page has two different forms and only one can be submitted-> dictated by sex.
+		process form according to sex
+		and assign the size id to size_id
+		"""
 		if sex == 'F':
 			femaleSize_form = FemaleSizeForm(request.POST)
 			if femaleSize_form.is_valid():
 				femaleSize_form_data = femaleSize_form.save()
 
 				size_id = femaleSize_form_data.id
+			else:
+				context['femaleSize_form'] = femaleSize_form
+
 
 		elif(sex == 'M'):
 			maleSize_form = MaleSizeForm(request.POST)
@@ -159,11 +181,14 @@ def order(request):
 				maleSize_form_data = maleSize_form.save()
 
 				size_id = maleSize_form_data.id
+			else:
+				context['maleSize_form'] = maleSize_form
+		# size is saved in the order table
 		size = Size.objects.get(id=size_id)
 
-	#save order
+	#prepare order and save
 		order_form = OrderForm(request.POST)
-		if order_form.is_valid():
+		if femaleSize_form.is_valid() or maleSize_form.is_valid() and order_form.is_valid():
 			order_form_data = order_form.save(commit=False)
 			order_form_data.client = client
 			order_form_data.size = size
