4df39a0f8e3e81f982e3e6ca112fedc5f65b27f3
avoajaugochukwu <avoajaugochukwu@github.com>
2015-03-26 14:15:31 +0100

    unwanted changes

--- suave/views.py
+++ suave/views.py
@@ -283,8 +283,8 @@ def tailorOrderDetails(request, main_order_id):
 	context['tailorPage'] = True
 	context['order'] = order
 
-	context['size'] = Size.objects.filter(id=order.size.id).values()
-	# context['size'] = serializers.serialize("python", Size.objects.filter(id=order.size.id))
+	# context['size'] = Size.objects.filter(id=order.size.id).values()
+	context['size'] = serializers.serialize("python", Size.objects.filter(id=order.size.id))
 	return render(request, 'i/tailor/order_details.html', context)
 
 
