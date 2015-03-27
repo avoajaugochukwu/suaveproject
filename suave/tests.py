4e68f90946d3a9167b0e86a7e1ba7b4342baf480
avoajaugochukwu <avoajaugochukwu@github.com>
2015-03-27 19:47:28 +0100

    unit testing -- Test view and clientRegistration

--- suave/tests.py
+++ suave/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from suave.models import Client
 
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class HomePageTest(TestCase):
+	def test_case(self):
+		resp = self.client.get('/suave/')
+		self.assertEqual(resp.status_code, 200)
+
+class ClientRegisterTest(TestCase):
+
+	def test_Case(self):
+		one = User.objects.create(username="testCase")
+		user = Client.objects.create(user=one, sex='F')
+		testCase = Client.objects.get(user=user)
+
+		self.assertEqual(testCase.sex, 'F')
+
+	def test_2Case(self):
+		one = User.objects.create(username="test2Case")
+		user = Client.objects.create(user=one, sex='M')
+		testCase = Client.objects.get(user=user)
+
+		self.assertEqual(testCase.sex, 'F')
